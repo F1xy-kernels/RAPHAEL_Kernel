0d1bed18996792d305ae5f0a302d50148e3118f8
DevriesL <therkduan@gmail.com>
2018-12-31 21:42:21 +0800

    drm: add get_main_display
    Signed-off-by: DevriesL <therkduan@gmail.com>
    Signed-off-by: Nanda Oktavera <codeharuka.yusa@gmail.com>
    Signed-off-by: Nauval Rizky <enuma.alrizky@gmail.com>

--- drivers/gpu/drm/msm/dsi-staging/dsi_display.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_display.c
@@ -61,6 +61,10 @@ static const struct of_device_id dsi_display_dt_match[] = {
 	{}
 };
 
+#ifdef CONFIG_EXPOSURE_ADJUSTMENT
+struct dsi_display *primary_display;
+#endif
+
 static void dsi_display_mask_ctrl_error_interrupts(struct dsi_display *display,
 			u32 mask, bool enable)
 {
@@ -6333,6 +6337,9 @@ int dsi_display_get_modes(struct dsi_display *display,
 exit:
 	*out_modes = display->modes;
 	rc = 0;
+#ifdef CONFIG_EXPOSURE_ADJUSTMENT
+	primary_display = display;
+#endif
 
 error:
 	if (rc)
@@ -7677,6 +7684,14 @@ int dsi_display_unprepare(struct dsi_display *display)
 	SDE_EVT32(SDE_EVTLOG_FUNC_EXIT);
 	return rc;
 }
+
+#ifdef CONFIG_EXPOSURE_ADJUSTMENT
+struct dsi_display *get_main_display(void)
+{
+	return primary_display;
+}
+EXPORT_SYMBOL(get_main_display);
+#endif
 
 static int __init dsi_display_register(void)
 {
