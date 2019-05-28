7a062abd11b3ba09fc76e0077cc4f72b4ffd24d0
Yaroslav Furman <yaro330@gmail.com>
2019-05-28 17:53:39 +0300

    drivers: fix numerous misleading indentation warnings
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

--- drivers/crypto/msm/qcedev.c
+++ drivers/crypto/msm/qcedev.c
@@ -1806,6 +1806,7 @@ static inline long qcedev_ioctl(struct file *file,
 					sizeof(struct qcedev_sha_op_req))) {
 			err = -EFAULT;
 			goto exit_free_qcedev_areq;
+			}
 		}
 		}
 		break;
@@ -1899,6 +1900,7 @@ static inline long qcedev_ioctl(struct file *file,
 					sizeof(struct qcedev_sha_op_req))) {
 			err = -EFAULT;
 			goto exit_free_qcedev_areq;
+			}
 		}
 		}
 		break;
