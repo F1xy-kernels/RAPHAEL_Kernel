fc74abd85480e9d5341e1f940c4bc3a48df18dd9
Yaroslav Furman <yaro330@gmail.com>
2019-06-01 17:47:42 +0300

    Revert "STOPSHIP: vdso: disable switches for experiments"
    We are actually shipping this and are not planning any experiments.
    
    This reverts commit 68c3f9d85ce756b0c0f7160e1d31a0694ad5b289.
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

--- arch/arm64/kernel/vdso.c
+++ arch/arm64/kernel/vdso.c
@@ -28,7 +28,6 @@
 #include <linux/gfp.h>
 #include <linux/kernel.h>
 #include <linux/mm.h>
-#include <linux/moduleparam.h>
 #include <linux/sched.h>
 #include <linux/signal.h>
 #include <linux/slab.h>
@@ -39,19 +38,6 @@
 #include <asm/signal32.h>
 #include <asm/vdso.h>
 #include <asm/vdso_datapage.h>
-
-#ifdef USE_SYSCALL
-#if defined(__LP64__)
-static int enable_64 = 1;
-module_param(enable_64, int, 0600);
-MODULE_PARM_DESC(enable_64, "enable vDSO for aarch64 0=off");
-#endif
-#if (!defined(__LP64) || (defined(CONFIG_COMPAT) && defined(CONFIG_VDSO32)))
-static int enable_32 = 1;
-module_param(enable_32, int, 0600);
-MODULE_PARM_DESC(enable_32, "enable vDSO for aarch64 0=off");
-#endif
-#endif
 
 struct vdso_mappings {
 	unsigned long num_code_pages;
@@ -340,23 +326,6 @@ void update_vsyscall(struct timekeeper *tk)
 {
 	u32 use_syscall = !tk->tkr_mono.clock->archdata.vdso_direct;
 
-#ifdef USE_SYSCALL
-	if (use_syscall) {
-		use_syscall = USE_SYSCALL | USE_SYSCALL_32 | USE_SYSCALL_64;
-	} else {
-#if (defined(__LP64__))
-		if (!enable_64)
-#endif
-			use_syscall = USE_SYSCALL_64;
-#if (!defined(__LP64) || (defined(CONFIG_COMPAT) && defined(CONFIG_VDSO32)))
-		if (!enable_32)
-#endif
-			use_syscall |= USE_SYSCALL_32;
-		if (use_syscall == (USE_SYSCALL_32 | USE_SYSCALL_64))
-			use_syscall |= USE_SYSCALL;
-	}
-#endif
-
 	++vdso_data->tb_seq_count;
 	smp_wmb();
 
@@ -370,11 +339,7 @@ void update_vsyscall(struct timekeeper *tk)
 	/* Read without the seqlock held by clock_getres() */
 	WRITE_ONCE(vdso_data->hrtimer_res, hrtimer_resolution);
 
-#ifdef USE_SYSCALL
-	if (!(use_syscall & USE_SYSCALL)) {
-#else
 	if (!use_syscall) {
-#endif
 		struct timespec btm = ktime_to_timespec(tk->offs_boot);
 
 		/* tkr_mono.cycle_last == tkr_raw.cycle_last */
