ec3de916b01745fcfb3f7eda4182afffec829a50
Yaroslav Furman <yaro330@gmail.com>
2019-12-23 00:52:55 +0300

    Revert unwanted apps blocker
    This was never meant to be shipped and was a POC commit.
    It will be refined and improved to better account for
    optimizers that can launch via Magisk.
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1225,23 +1225,6 @@ char *__get_task_comm(char *buf, size_t buf_size, struct task_struct *tsk)
 }
 EXPORT_SYMBOL_GPL(__get_task_comm);
 
-#ifdef CONFIG_BLOCK_UNWANTED_APPS
-struct task_kill_info {
-	struct task_struct *task;
-	struct work_struct work;
-};
-
-static void proc_kill_task(struct work_struct *work)
-{
-	struct task_kill_info *kinfo = container_of(work, typeof(*kinfo), work);
-	struct task_struct *task = kinfo->task;
-
-	send_sig(SIGKILL, task, 0);
-	put_task_struct(task);
-	kfree(kinfo);
-}
-#endif
-
 /*
  * These functions flushes out all traces of the currently running executable
  * so that a new one can be started
@@ -1253,25 +1236,6 @@ void __set_task_comm(struct task_struct *tsk, const char *buf, bool exec)
 	trace_task_rename(tsk, buf);
 	strlcpy(tsk->comm, buf, sizeof(tsk->comm));
 	task_unlock(tsk);
-
-#ifdef CONFIG_BLOCK_UNWANTED_APPS
-	if (unlikely(strstr(tsk->comm, "lspeed")) ||
-		unlikely(strstr(tsk->comm, "fde")) ||
-		unlikely(!strcmp(tsk->comm, "nfs1")) ||
-		unlikely(!strcmp(tsk->comm, "nfs2")) ||
-		unlikely(strstr(tsk->comm, "projectkr"))) {
-		struct task_kill_info *kinfo;
-		pr_info("%s: blocking %s\n", __func__, tsk->comm);
-		kinfo = kmalloc(sizeof(*kinfo), GFP_KERNEL);
-		if (kinfo) {
-			get_task_struct(tsk);
-			kinfo->task = tsk;
-			INIT_WORK(&kinfo->work, proc_kill_task);
-			schedule_work(&kinfo->work);
-		}
-	}
-#endif
-
 	perf_event_comm(tsk, exec);
 }
 
