b1c74200ddc177740ad7956bb7a75f379c04b66b
Yaroslav Furman <yaro330@gmail.com>
2020-01-20 04:09:32 +0300

    techpack: shut the fuck up
    Shut the fuck up
    You're a fucking cunt
    Shut the fuck up
    You're a stupid cunt, suck my dick
    Shut the fuck up
    Stop being a fucking cunt
    Shut the fuck up
    Nobody even wants you here
    What?
    I just want to let you know, you're a stupid fucking cunt
    Go ahead and run your mouth, pussy, I don't give a fuck
    You're a stupid piece of shit, you're a stupid fucking bitch
    Get the fuck up off my dick, get the fuck up off my dick, like
    Please end your fucking life, please end your fucking life
    I really gotta emphasise, no one cares if you're alive
    You're a fucking penis-hole, grab a dick and eat it whole
    I need to know if you were dropped when you were just a fetus though
    You're so fucking ugly and your face is fucking foul, jeez
    You're so fucking loud, can you shut your fucking mouth, can you
    Shut the fuck up
    You're a fucking cunt
    Shut the fuck up
    You're a stupid cunt, suck my dick
    Shut the fuck up
    Stop being a fucking cunt
    Shut the fuck up
    Nobody even wants you here
    Yeah
    Close your fucking mouth, you're just really fucking dense
    If you hate me, why you talking? You don't make no fucking sense
    Got a sad life, sad life, go to fucking hell
    Are you stupid or disabled, man, I can't fucking tell
    Meh
    You're a fucking dumb shit, you don't even run shit
    Get the fuck up out of my face and go to hell and eat a dick
    Come and catch these hands, boy
    Come and match these bands, boy
    I'm not crazy, I just do it all because I can, boy
    I hope you fucking die in a high-speed car crash
    I hope you fucking fall head-first and get your neck cracked
    I hope you have some beautiful children that die from cancer
    I hope you catch Zika when your wife gets pregnant
    I hope you win the lottery and die the next day
    And your daughter has to see you getting lowered in your grave
    Like, uh, ooh- that was a little dark
    I'm sorry. Tha-that was a little dark. Very poor taste
    Shut the fuck up
    I shouldn't have said that
    You're a fucking cunt
    Actually, no, I should have
    Shut the fuck up
    Actually, I didn't say enough
    You're a stupid cunt, suck my dick
    Shut the fuck up
    Stop being a fucking cunt
    Shut the fuck up
    Nobody even wants you here
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

--- techpack/audio/asoc/codecs/tfa98xx/inc/dbgprint.h
+++ techpack/audio/asoc/codecs/tfa98xx/inc/dbgprint.h
@@ -19,9 +19,9 @@
 #   define _DBGPRINT_H
 
 /* Debugging macro's. */
-#   ifndef DEBUG
-#      define DEBUG
-#   endif
+//#   ifndef DEBUG
+//#      define DEBUG
+//#   endif
 
 #   ifndef ASSERT
 //#define ASSERT
