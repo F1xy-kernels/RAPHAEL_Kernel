e49abfcc0e03e7beada828f54a3c99c5115767d7
Yaroslav Furman <yaro330@gmail.com>
2020-04-13 12:11:01 +0300

    Revert "drm/msm/sde: use kmem_cache pool for struct sde_fence"
    There has been a few SDE related errors lately, let's try to debug them.
    
    This reverts commit fa3b0eafe817d02b10209b29ac45cb1d40db3d95.
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

--- drivers/gpu/drm/msm/sde/sde_fence.c
+++ drivers/gpu/drm/msm/sde/sde_fence.c
@@ -19,8 +19,6 @@
 
 #define TIMELINE_VAL_LENGTH		128
 
-static struct kmem_cache *kmem_fence_pool;
-
 void *sde_sync_get(uint64_t fd)
 {
 	/* force signed compare, fdget accepts an int argument */
@@ -163,7 +161,7 @@ static void sde_fence_release(struct dma_fence *fence)
 	if (fence) {
 		f = to_sde_fence(fence);
 		kref_put(&f->ctx->kref, sde_fence_destroy);
-		kmem_cache_free(kmem_fence_pool, f);
+		kfree(f);
 	}
 }
 
@@ -216,7 +214,7 @@ static int _sde_fence_create_fd(void *fence_ctx, uint32_t val)
 		goto exit;
 	}
 
-	sde_fence = kmem_cache_zalloc(kmem_fence_pool, GFP_KERNEL);
+	sde_fence = kzalloc(sizeof(*sde_fence), GFP_KERNEL);
 	if (!sde_fence)
 		return -ENOMEM;
 
@@ -520,11 +518,3 @@ void sde_debugfs_timeline_dump(struct sde_fence_context *ctx,
 	}
 	spin_unlock(&ctx->list_lock);
 }
-
-static int __init sde_kmem_pool_init(void)
-{
-	kmem_fence_pool = KMEM_CACHE(sde_fence, SLAB_HWCACHE_ALIGN | SLAB_PANIC);
-	return 0;
-}
-
-module_init(sde_kmem_pool_init);
