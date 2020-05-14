e6b0ec2dee308386e1445f015ab4c7d379a6a67e
Yaroslav Furman <yaro330@gmail.com>
2020-05-15 00:42:15 +0300

    techpack: compile out elliptic garbage
    We don't need it.
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

--- techpack/audio/dsp/q6_init.c
+++ techpack/audio/dsp/q6_init.c
@@ -37,7 +37,9 @@ static int __init audio_q6_init(void)
 #endif
 	msm_mdf_init();
 	voice_mhi_init();
+#if 0
 	elliptic_driver_init();
+#endif
 /* for mius start */
 #ifdef CONFIG_US_PROXIMITY
 	mius_driver_init();
@@ -66,7 +68,9 @@ static void __exit audio_q6_exit(void)
 	audio_cal_exit();
 	adsp_err_exit();
 	voice_mhi_exit();
+#if 0
 	elliptic_driver_exit();
+#endif
 /* for mius start */
 #ifdef CONFIG_US_PROXIMITY
 	mius_driver_exit();
