d1108a63f1431db28adce7d8159189b59b01b097
Mohammed Nayeem Ur Rahman <mohara@codeaurora.org>
2020-06-23 20:22:59 +0530

    msm: adsprpc: Reduce logging in map/unmap IOCTLs
    In map/unmap IOCTLs, removing the error log printed in case
    process is not initialized as it can lead to excessive logging
    in fuzzy IOCTL testing. These calls, however, are returing proper
    error codes which can be checked in userspace for error.
    
    Change-Id: I702d3ca2916675d6b067ffb0d0c914719de08bc4
    Acked-by: Maitreyi Gupta <maitreyi@qti.qualcomm.com>
    Signed-off-by: Mohammed Nayeem Ur Rahman <mohara@codeaurora.org>
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>

--- drivers/char/adsprpc.c
+++ drivers/char/adsprpc.c
@@ -2643,8 +2643,6 @@ static int fastrpc_internal_munmap(struct fastrpc_file *fl,
 
 	VERIFY(err, fl->dsp_proc_init == 1);
 	if (err) {
-		pr_err("adsprpc: ERROR: %s: user application %s trying to unmap without initialization\n",
-			 __func__, current->comm);
 		err = -EBADR;
 		return err;
 	}
@@ -2710,8 +2708,6 @@ static int fastrpc_internal_munmap_fd(struct fastrpc_file *fl,
 		return err;
 	VERIFY(err, fl->dsp_proc_init == 1);
 	if (err) {
-		pr_err("adsprpc: ERROR: %s: user application %s trying to unmap without initialization\n",
-			__func__, current->comm);
 		err = -EBADR;
 		return err;
 	}
@@ -2747,8 +2743,6 @@ static int fastrpc_internal_mmap(struct fastrpc_file *fl,
 
 	VERIFY(err, fl->dsp_proc_init == 1);
 	if (err) {
-		pr_err("adsprpc: ERROR: %s: user application %s trying to map without initialization\n",
-			__func__, current->comm);
 		err = -EBADR;
 		return err;
 	}
