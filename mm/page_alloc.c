90dbf2bffc1386385fdfedbb455dad802d095a3c
Yaroslav Furman <yaro330@gmail.com>
2021-07-11 20:27:13 +0300

    Revert "mm: Boost when memory pressure becomes high"
    This reverts commit 357f02c3f455704e235b463951407ee138e87513.
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -71,8 +71,6 @@
 #include <linux/nmi.h>
 #include <linux/khugepaged.h>
 #include <linux/psi.h>
-#include <linux/cpu_input_boost.h>
-#include <linux/devfreq_boost.h>
 
 #include <asm/sections.h>
 #include <asm/tlbflush.h>
@@ -4273,12 +4271,6 @@ retry:
 	 */
 	if (costly_order && !(gfp_mask & __GFP_RETRY_MAYFAIL))
 		goto nopage;
-
-	/* Boost when memory is low so allocation latency doesn't get too bad */
-	cpu_input_boost_kick_max(100);
-	devfreq_boost_kick_max(DEVFREQ_MSM_LLCCBW, 100);
-	devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 100);
-	pr_info_ratelimited("%s: boosting\n", __func__);
 
 	if (should_reclaim_retry(gfp_mask, order, ac, alloc_flags,
 				 did_some_progress > 0, &no_progress_loops))
